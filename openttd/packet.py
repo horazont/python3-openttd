d85c52bfdf6e5f8ebc9c561f36db62ccacc5bbcb
Jonas Wielicki <j.wielicki@sotecware.net>
2014-11-24 15:30:14 +0100

    Add license headers

--- openttd/packet.py
+++ openttd/packet.py
@@ -1,3 +1,26 @@
+# File name: packet.py
+# This file is part of: python3-openttd
+#
+# LICENSE
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# FEEDBACK & QUESTIONS
+#
+# For feedback and questions about python3-openttd please e-mail one of
+# the authors named in the AUTHORS file.
+########################################################################
+
 """
 .. _packet:
 
