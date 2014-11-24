63c76b77ab6cb5c1b9d93eb8471c5f3b8adc94fb
Jonas Wielicki <j.wielicki@sotecware.net>
2014-11-24 12:40:46 +0100

    Setup setuptools ☺

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name="python3-openttd",
+      version="0.1",
+      description="OpenTTD administration client library",
+      author="Jonas Wielicki",
+      author_email="jonas@wielicki.name",
+      url="https://github.com/horazont/python3-openttd",
+      packages=["openttd"],
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3 Only",
+          "Programming Language :: Python :: 3.4",
+          "Topic :: Games/Entertainment :: Simulation",
+      ],
+)
---
+++ utils/convert_enum.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+Helper script to convert a C++ enum to a Python3 enum.
+"""
+import re
+
+def parse_values(lines):
+    decl_re = re.compile(
+        r"(?P<name>[A-Z_]+)(\s*=\s*(?P<value>([0-9]+|0x[0-9a-fA-F]+)))?\s*,(\s*///<\s*(?P<comment>.*))?"
+        # r"[A-Z_].*"
+    )
+
+    for line in lines:
+        line = line.strip()
+        match = decl_re.match(line)
+        if not match:
+            continue
+
+        named_groups = match.groupdict()
+        name = named_groups["name"]
+        value = named_groups["value"]
+        comment = named_groups["comment"]
+        if value is not None:
+            if value.startswith("0x"):
+                value = int(value[2:], 16)
+            else:
+                value = int(value)
+
+        yield name, value, comment
+
+def strip_prefixes(decls, prefix):
+    prefixlen = len(prefix)
+    for name, value, comment in decls:
+        if name.startswith(prefix):
+            name = name[prefixlen:]
+        yield name, value, comment
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s", "--strip-prefix",
+        default=None,
+        help="Prefix to strip from enumeration values"
+    )
+    parser.add_argument(
+        "-i", "--indent",
+        type=int,
+        default=1,
+        help="Level of indent for the output"
+    )
+
+    args = parser.parse_args()
+    if args.indent < 0:
+        print("negative indent? you’re insane.", file=sys.stderr)
+        sys.exit(1)
+
+    decls = parse_values(sys.stdin)
+    if args.strip_prefix is not None:
+        decls = strip_prefixes(decls, args.strip_prefix)
+
+    indent = "    "*args.indent
+
+    prev_value = -1
+    for name, value, comment in decls:
+        if value is None:
+            value = prev_value + 1
+
+        if comment is not None:
+            print("{indent}# {}".format(comment, indent=indent))
+        print("{indent}{} = {}".format(name, value, indent=indent))
+
+        prev_value = value
